abeb551a47c4d012f08fb754e73e5d46384ac4c1
Noah Wright <nwright5252@gmail.com>
2023-04-12 21:00:13 -0500

    book assignment with react

---
+++ week10cb/realDay3/server/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+const Book = require('./schemas/book')
+
+mongoose.connect('mongodb+srv://nwright5252:j8bNPn3toSHK0T67@cluster0.xmz6isa.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+}).catch((error) => {
+    console.log(error)
+})
+
+app.post('/api/add-book', async (req,res) => {
+    const bookTitle = req.body.bookTitle
+    const bookGenre = req.body.bookGenre
+    const bookPublisher = req.body.bookPublisher
+    const bookYear = req.body.bookYear
+    const bookImageURL = req.body.bookImageURL
+
+    const book = new Book({
+        bookTitle:bookTitle,
+        bookGenre:bookGenre,
+        bookPublisher:bookPublisher,
+        bookYear:bookYear,
+        bookImageURL:bookImageURL
+    })
+    await book.save()
+    res.json(book)
+})
+
+app.post('/api/books', async (req,res) => {
+    const books = await Book.find({})
+    res.json(books)
+})
+
+app.listen(8080, () => {
+    console.log('server running')
+})
